6bf2859b8d42cb7ecde9b2ba9eb40d414fa86ba7
Rodrigo H. Imai <rhidkusp@usp.br>
2025-11-17 17:27:00 -0300

    Criando a base do site

--- anime_blog/posts/models.py
+++ anime_blog/posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField("Título do Anime ou Episódio", max_length=255)
+    content = models.TextField("Conteúdo em HTML (Análise, Review ou Curiosidades)")
+    posted_at = models.DateTimeField("Data da postagem", auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
