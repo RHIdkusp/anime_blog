79f8a0b9f6ab8c988c6ac3eb5a344f2ccac4b9ec
Rodrigo H. Imai <rhidkusp@usp.br>
2025-11-17 17:32:44 -0300

    Views funcionais usando Django Form

--- anime_blog/posts/views.py
+++ anime_blog/posts/views.py
@@ -1,61 +1,54 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
-# ---------------------------
 # LISTAGEM
-# ---------------------------
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "post_list.html", {"posts": posts})
 
 
-# ---------------------------
 # DETALHES (COM 404)
-# ---------------------------
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "post_detail.html", {"post": post})
 
 
-# ---------------------------
-# CRIAÇÃO DE POST (SEM FORM)
-# ---------------------------
+# CRIAÇÃO COM FORM
 def post_create(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
+        form = PostForm(request.POST)
 
-        Post.objects.create(
-            title=title,
-            content=content,
-        )
+        if form.is_valid():
+            form.save()
+            return redirect("post_list")
 
-        return redirect("post_list")
+    else:
+        form = PostForm()
 
-    return render(request, "post_form.html", {"post": None, "form": None})
+    return render(request, "post_form.html", {"form": form, "post": None})
 
 
-# ---------------------------
-# ATUALIZAÇÃO DE POST (SEM FORM)
-# ---------------------------
+# ATUALIZAÇÃO COM FORM
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "POST":
-        post.title = request.POST.get("title")
-        post.content = request.POST.get("content")
-        post.save()
+        form = PostForm(request.POST, instance=post)
 
-        return redirect("post_detail", pk=post.pk)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=pk)
 
-    return render(request, "post_form.html", {"post": post, "form": None})
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "post_form.html", {"form": form, "post": post})
 
 
-# ---------------------------
-# REMOÇÃO (COM PÁGINA DE CONFIRMAÇÃO)
-# ---------------------------
+# REMOÇÃO (COM CONFIRMAÇÃO)
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
