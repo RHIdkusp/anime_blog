d09f39abf0931ecff7698ec212ca42f40a3f7c99
Rodrigo H. Imai <rhidkusp@usp.br>
2025-11-19 17:59:40 -0300

    Usando fallback no DATABASES

--- anime_blog/settings.py
+++ anime_blog/settings.py
@@ -75,20 +75,13 @@ WSGI_APPLICATION = 'anime_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.getenv("RENDER"):
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    "default": dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
