a8e81b962e99697ee2a5bc0edbdae3e06a16438d
Rodrigo H. Imai <rhidkusp@usp.br>
2025-11-19 10:11:19 -0300

    Registrando o modelo no admin e criando o formulário opcional de comentário

--- anime_blog/posts/admin.py
+++ anime_blog/posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 # Register your models here.
 
@@ -7,3 +8,5 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'posted_at')
     search_fields = ('title', 'content')
+    
+admin.site.register(Comment)
--- anime_blog/posts/models.py
+++ anime_blog/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -16,3 +17,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', kwargs={'pk': self.pk})
+    
+User = get_user_model()
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário de {self.author} em {self.post}"
