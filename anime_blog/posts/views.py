87ab1aee915791ef3591a83341346f27883f7b2e
Rodrigo H. Imai <rhidkusp@usp.br>
2025-11-19 10:16:05 -0300

    Criando as views de comentário

--- anime_blog/posts/views.py
+++ anime_blog/posts/views.py
@@ -1,8 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
 
 
 # --------------------------
@@ -21,6 +24,13 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
     context_object_name = "post"
+
+    # Enviar comentários para o template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Comentários mais recentes primeiro
+        context["comments"] = self.object.comments.order_by("-date_posted")
+        return context
 
 
 # --------------------------
@@ -52,3 +62,24 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = "post_confirm_delete.html"
     success_url = reverse_lazy("post_list")
+
+
+# ========================================
+# ADICIONAR COMENTÁRIOS (PASSO 5)
+# ========================================
+@login_required
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(request, "post_add_comment.html", {"form": form, "post": post})
